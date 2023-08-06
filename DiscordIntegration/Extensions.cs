eb3e14305eef7b8d1b9af0a6bb9b13d717f10ae4
swd <swdchw@gmail.com>
2023-08-06 20:36:40 +0500

    Я люблю ебал региксы

--- DiscordIntegration/Extensions.cs
+++ DiscordIntegration/Extensions.cs
@@ -5,7 +5,7 @@ namespace DiscordIntegration;
 using API.Commands;
 public static class Extensions
 {
-    public static bool IsValidUserId(this string userId) => Regex.IsMatch(userId, "^([0-9]{17})@(steam|patreon|northwood)|([0-9]{19})@(discord)$");
+    public static bool IsValidUserId(this string userId) => Regex.IsMatch(userId, "^([0-9]{17})@(steam|patreon|northwood)|^[0-9]{18,19}@discord$");
     public static bool IsValidDiscordId(this string discordId) => Regex.IsMatch(discordId, "^[0-9]{18,19}$");
     public static bool IsValidDiscordRoleId(this string discordRoleId) => IsValidDiscordId(discordRoleId);
     public static CommandSender GetCompatible(this ICommandSender sender) => ((CommandSender)sender).GetCompatible();
